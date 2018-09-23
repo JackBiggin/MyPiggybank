6c35f3693c3d597ecd7ab38cd173c6374c92b25d
Jack Biggin <me@jackbiggin.me>
2018-09-23 03:18:58 -0400

    Auto stash before merge of "master" and "origin/master"

--- transactionList.js
+++ transactionList.js
@@ -145,7 +145,8 @@ function updateDateFilter() {
 }
 
 function loadLargeTransactionArray(largeArray) {
-	jQuery.getJSON("/backend/get_transactions.php", function(json) {
-		largeTransactionArray = json;
+	jQuery.getJSON("./backend/get_transactions.php", function(json) {
+		console.log(json.transactions);
+
 	});
 }
