0922fea5024bb1d2d6154896b6117ebdb7229348
andrewdimmer <43498863+andrewdimmer@users.noreply.github.com>
2018-09-23 03:22:04 -0400

    Json Fix 3:22

--- transactionList.js
+++ transactionList.js
@@ -146,6 +146,6 @@ function updateDateFilter() {
 
 function loadLargeTransactionArray(largeArray) {
 	jQuery.getJSON("/backend/get_transactions.php", function(json) {
-		largeTransactionArray = json;
+		largeTransactionArray = [json.transactions];
 	});
 }
